0abee3b388e5ba7733347dd683ed4fa80c645942
Víctor Pasalamar <victorpasalamar@iesebre.com>
2023-09-19 13:14:41 +0200

    Entrega projecte 1

---
+++ Projecte_1/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        /*Vaig a mostrar per pantalla el missatge "Jelou uold"
+        Para posar la comanda de baix rapid se fa ent -->sout
+        */
+        System.out.println("Jelou uold");
+        System.out.print("jelou uold 2 ");
+        System.out.println("electric boogaloo");
+        System.out.print("jelou uold ");
+        System.out.print("3 ");
+        System.out.println("ahora es personal ");
+    }
+}
